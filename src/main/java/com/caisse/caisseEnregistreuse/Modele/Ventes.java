334ea6261bc02cef93f5c9757bde2de715eefd34
MohKoureissi <koureissi89@gmail.com>
2024-07-23 18:02:47 +0000

    mm

--- src/main/java/com/caisse/caisseEnregistreuse/Modele/Ventes.java
+++ src/main/java/com/caisse/caisseEnregistreuse/Modele/Ventes.java
@@ -1,12 +1,12 @@
 package com.caisse.caisseEnregistreuse.Modele;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDate;
 
 @Data
 @Entity
@@ -16,5 +16,16 @@ public class Ventes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idVente;
-    
+@Column(nullable = false, name = "dateVente")
+@NotNull(message = "Ce champs est vide")
+private LocalDate dateVente;
+@Column(nullable = false, name = "montant")
+@NotNull(message = "Ce champs est vide")
+private long montant;
+
+    @ManyToOne()
+    @JoinColumn(name = "idUtilisateur")
+    private Utilisateur utilisateur;
+
+
 }
